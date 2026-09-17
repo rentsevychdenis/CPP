67c71a6c5c71d6ad530f8ce4f6cc7ea0ecd40c5c
rentsevychdenis <rentsevych.denis@lll.kpi.ua>
2026-09-17 11:10:31 +0300

    Add files via upload

---
+++ Lab.3/Lab.3.1.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <chrono>
+
+using namespace std;
+
+const int ARRAY_SIZE = 100000000;
+vector<int> dataArray;
+long long globalSum = 0;
+
+HANDLE hMutex;
+HANDLE hStartEvent;
+HANDLE hConsoleMutex;
+
+struct ThreadData {
+    int startIdx;
+    int endIdx;
+};
+
+void SafePrint(const string& msg, DWORD threadId) {
+    WaitForSingleObject(hConsoleMutex, INFINITE);
+    cout << "[Потік " << threadId << "] " << msg << "\n";
+    ReleaseMutex(hConsoleMutex);
+}
+
+DWORD WINAPI WorkerProc(LPVOID lpParam) {
+    ThreadData* td = (ThreadData*)lpParam;
+    DWORD tid = GetCurrentThreadId();
+
+    SafePrint("Створено. Очікування команди на старт...", tid);
+
+    WaitForSingleObject(hStartEvent, INFINITE);
+    SafePrint("Почав обчислення своєї частини масиву.", tid);
+
+    long long localSum = 0;
+    for (int i = td->startIdx; i < td->endIdx; ++i) {
+        localSum += dataArray[i];
+    }
+
+    SafePrint("Очікування доступу до глобальної суми (Mutex)...", tid);
+    WaitForSingleObject(hMutex, INFINITE);
+
+    SafePrint("Доступ отримано. Оновлення глобальної суми.", tid);
+    globalSum += localSum;
+
+    ReleaseMutex(hMutex);
+
+    SafePrint("Завершує роботу.", tid);
+
+    ExitThread(0);
+    return 0;
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    cout << "Ініціалізація масиву (" << ARRAY_SIZE << " елементів)...\n";
+    dataArray.assign(ARRAY_SIZE, 1);
+
+    hMutex = CreateMutex(NULL, FALSE, NULL);
+    hConsoleMutex = CreateMutex(NULL, FALSE, NULL);
+    hStartEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    int numThreads;
+    cout << "\nВведіть кількість потоків (наприклад: 2, 4, 8 або 16): ";
+    cin >> numThreads;
+
+    if (numThreads <= 0 || numThreads > 64) {
+        cout << "Некоректна кількість потоків.\n";
+        return 1;
+    }
+
+    globalSum = 0;
+    ResetEvent(hStartEvent);
+
+    vector<HANDLE> hThreads(numThreads);
+    vector<ThreadData> tData(numThreads);
+
+    int chunkSize = ARRAY_SIZE / numThreads;
+
+    for (int i = 0; i < numThreads; ++i) {
+        tData[i].startIdx = i * chunkSize;
+        tData[i].endIdx = (i == numThreads - 1) ? ARRAY_SIZE : (i + 1) * chunkSize;
+
+        hThreads[i] = CreateThread(NULL, 0, WorkerProc, &tData[i], 0, NULL);
+    }
+
+    cout << "\nУсі потоки створені. Натисніть Enter для початку обчислень...";
+    cin.ignore();
+    cin.get();
+
+    auto startTime = chrono::high_resolution_clock::now();
+
+    SetEvent(hStartEvent);
+
+    WaitForMultipleObjects(numThreads, hThreads.data(), TRUE, INFINITE);
+
+    auto endTime = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(endTime - startTime);
+
+    cout << "\n=== РЕЗУЛЬТАТИ ===\n";
+    cout << "Обчислена сума: " << globalSum << " (Очікувалося: " << ARRAY_SIZE << ")\n";
+    cout << "Час виконання: " << duration.count() << " мс\n";
+
+    for (int i = 0; i < numThreads; ++i) {
+        CloseHandle(hThreads[i]);
+    }
+    CloseHandle(hMutex);
+    CloseHandle(hConsoleMutex);
+    CloseHandle(hStartEvent);
+
+    return 0;
+}
---
+++ Lab.3/Lab.3.cpp
@@ -0,0 +1,133 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+#define ID_THREADS_START 1001
+#define ID_THREADS_STOP  1002
+
+struct GlobalTime {
+    WORD hour;
+    WORD minute;
+    WORD second;
+};
+
+GlobalTime g_time;
+CRITICAL_SECTION g_cs;
+HWND g_hWnd;
+HANDLE hThreads[2] = { NULL, NULL };
+bool g_stopThreads = false;
+char szTimeStr[50] = "Press Threads -> Start";
+
+DWORD WINAPI WriterThread(LPVOID lpParam) {
+    while (!g_stopThreads) {
+        SYSTEMTIME st;
+        GetLocalTime(&st);
+
+        EnterCriticalSection(&g_cs);
+        g_time.hour = st.wHour;
+        g_time.minute = st.wMinute;
+        g_time.second = st.wSecond;
+        LeaveCriticalSection(&g_cs);
+
+        Sleep(500);
+    }
+    return 0;
+}
+
+DWORD WINAPI ReaderThread(LPVOID lpParam) {
+    while (!g_stopThreads) {
+        EnterCriticalSection(&g_cs);
+        sprintf_s(szTimeStr, "%02d:%02d:%02d", g_time.hour, g_time.minute, g_time.second);
+        LeaveCriticalSection(&g_cs);
+
+        InvalidateRect(g_hWnd, NULL, TRUE);
+        Sleep(1000);
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE: {
+        HMENU hMenu = CreateMenu();
+        HMENU hSubMenu = CreatePopupMenu();
+        AppendMenuA(hSubMenu, MF_STRING, ID_THREADS_START, "Start");
+        AppendMenuA(hSubMenu, MF_STRING, ID_THREADS_STOP, "Stop");
+        AppendMenuA(hMenu, MF_STRING | MF_POPUP, (UINT_PTR)hSubMenu, "Threads");
+        SetMenu(hWnd, hMenu);
+        break;
+    }
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case ID_THREADS_START:
+            if (hThreads[0] == NULL && hThreads[1] == NULL) {
+                g_stopThreads = false;
+                hThreads[0] = CreateThread(NULL, 0, WriterThread, NULL, 0, NULL);
+                hThreads[1] = CreateThread(NULL, 0, ReaderThread, NULL, 0, NULL);
+            }
+            break;
+        case ID_THREADS_STOP:
+            g_stopThreads = true;
+            if (hThreads[0]) {
+                WaitForSingleObject(hThreads[0], INFINITE);
+                CloseHandle(hThreads[0]);
+                hThreads[0] = NULL;
+            }
+            if (hThreads[1]) {
+                WaitForSingleObject(hThreads[1], INFINITE);
+                CloseHandle(hThreads[1]);
+                hThreads[1] = NULL;
+            }
+            break;
+        }
+        break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        RECT rt;
+        GetClientRect(hWnd, &rt);
+        DrawTextA(hdc, szTimeStr, -1, &rt, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        EndPaint(hWnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        g_stopThreads = true;
+        if (hThreads[0]) {
+            WaitForSingleObject(hThreads[0], INFINITE);
+            CloseHandle(hThreads[0]);
+        }
+        if (hThreads[1]) {
+            WaitForSingleObject(hThreads[1], INFINITE);
+            CloseHandle(hThreads[1]);
+        }
+        DeleteCriticalSection(&g_cs);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    InitializeCriticalSection(&g_cs);
+
+    WNDCLASSEXA wc = { sizeof(WNDCLASSEXA), CS_HREDRAW | CS_VREDRAW, WndProc, 0, 0, hInstance,
+                       NULL, LoadCursor(NULL, IDC_ARROW), (HBRUSH)(COLOR_WINDOW + 1),
+                       NULL, "Lab3WinClass", NULL };
+    RegisterClassExA(&wc);
+
+    g_hWnd = CreateWindowA("Lab3WinClass", "Lab 3 - Variant 2",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 300, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(g_hWnd, nCmdShow);
+    UpdateWindow(g_hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
